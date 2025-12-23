7120f1bc3f07f91e6f1c801f985dba03709febb7
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2025-12-23 17:19:34 +0600

    updated

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,7 +7,8 @@ import { Toaster } from "sonner";
 
 const Layout = () => {
   return (
-    <div className="bg-black  ">
+    <div className="bg-black relative  ">
+      <div className="absolute bottom-0 left-0 right-0 h-screen top-0 bg-[radial-gradient(#5c545433_1px,#000000_1px)] bg-[size:20px_20px] [mask-image:radial-gradient(ellipse_105%_90%_at_50%_0%,#000_45%,transparent_120%)]"></div>
       <Toaster richColors position="top-right" />
       <Preloader />
       <SmoothScrollProvider>
